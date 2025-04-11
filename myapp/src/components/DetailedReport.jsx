4ecba2c99238a1233f1856aaf783ee2b9f3e7248
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-11 23:31:26 +0700

    Cau3

--- myapp/src/components/DetailedReport.jsx
+++ myapp/src/components/DetailedReport.jsx
@@ -1,6 +1,33 @@
 import avatar from "../assets/avatar.png";
 import { Pencil } from "lucide-react";
+import "../styles/DetailedReportStyle.css";
 const DetailedReport = () => {
-  return <div></div>;
+  return (
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th> CustomerName</th>
+            <th>Company</th>
+            <th>OrderValue</th>
+            <th>OrderDate</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default DetailedReport;
