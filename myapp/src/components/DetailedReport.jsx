6bcf5e65737eae5b237dea4bd570536f12971723
phihungdeptraino2 <hungdz1372004@gmail.com>
2025-04-11 23:57:55 +0700

    Cau4

--- myapp/src/components/DetailedReport.jsx
+++ myapp/src/components/DetailedReport.jsx
@@ -1,7 +1,14 @@
 import avatar from "../assets/avatar.png";
 import { Pencil } from "lucide-react";
 import "../styles/DetailedReportStyle.css";
+import { useEffect, useState } from "react";
 const DetailedReport = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3001/report")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   return (
     <div className="table-container">
       <table>
@@ -16,15 +23,26 @@ const DetailedReport = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
+          {data.map((item, index) => (
+            <tr>
+              <td className="check-box">
+                <input type="checkbox" />
+              </td>
+
+              <td>
+                <div className="section">
+                  <img src={avatar} alt="" className="image" />
+                  {item.name}
+                </div>
+              </td>
+              <td>{item.company}</td>
+              <td>{item.value}</td>
+              <td>{item.date}</td>
+              <td className={`status-${item.status.toLowerCase()}`}>
+                {item.status}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
